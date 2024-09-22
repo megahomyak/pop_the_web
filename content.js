284f2f79b65c05baef86fe81626019dd9a8081fb
megahomyak <g.megahomyak@gmail.com>
2024-09-22 13:03:36 +0500

    Trying to fix the sound issue

--- content.js
+++ content.js
@@ -2,7 +2,7 @@
     if (browser === undefined) {
         var browser = chrome;
     }
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    const AudioContext = window.AudioContext// || window.webkitAudioContext;
     const audioContext = new AudioContext();
 
     const popAudioBuffer = await audioContext.decodeAudioData(
@@ -32,20 +32,24 @@
         if (!extension.isEnabled) {
             return;
         }
-        if (event.type === "pointerup") {
-            if (selectedElement !== document.documentElement) {
-                const audioBufferSource = audioContext.createBufferSource();
-                audioBufferSource.buffer = popAudioBuffer;
-                audioBufferSource.connect(audioContext.destination);
-                audioBufferSource.start();
-                selectedElement.remove();
-            }
-        } else if (event.type === "pointerdown") {
-            event.target.style["touch-action"] = "manipulation";
-            selectedElement = event.target;
-        }
         event.preventDefault();
         event.stopImmediatePropagation();
+        (async () => {
+            if (event.type === "pointerup") {
+                if (selectedElement !== document.documentElement) {
+                    const audioBufferSource = audioContext.createBufferSource();
+                    audioBufferSource.buffer = popAudioBuffer;
+                    audioBufferSource.connect(audioContext.destination);
+                    await audioContext.resume();
+                    audioBufferSource.start();
+                    selectedElement.remove();
+                    console.log(audioContext, audioBufferSource);
+                }
+            } else if (event.type === "pointerdown") {
+                event.target.style["touch-action"] = "manipulation";
+                selectedElement = event.target;
+            }
+        })();
     }
 
     `
