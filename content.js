6310857e61df802b0addebf8b1ce14135fbaff8a
megahomyak <g.megahomyak@gmail.com>
2024-09-22 09:48:10 +0500

    Partially fixed the issue of the page disappearing when scrolling with a hiding scrollbar on Linux

--- content.js
+++ content.js
@@ -12,17 +12,25 @@
     );
 
     let isEnabled = await browser.runtime.sendMessage({ type: "getIsEnabled" });
+    let scrollingOccured = false;
 
     function handleEvent(event) {
+        console.log(event.type);
         if (!isEnabled) {
             return;
         }
-        if (event.type === "pointerup" && event.target.remove !== undefined) {
-            const audioBufferSource = audioContext.createBufferSource();
-            audioBufferSource.buffer = popAudioBuffer;
-            audioBufferSource.connect(audioContext.destination);
-            audioBufferSource.start();
-            event.target.remove();
+        if (event.type === "pointerup") {
+            if (!scrollingOccured && event.target.remove !== undefined) {
+                const audioBufferSource = audioContext.createBufferSource();
+                audioBufferSource.buffer = popAudioBuffer;
+                audioBufferSource.connect(audioContext.destination);
+                audioBufferSource.start();
+                event.target.remove();
+            }
+        } else if (event.type === "pointerdown") {
+            scrollingOccured = false;
+        } else if (event.type === "scroll") {
+            scrollingOccured = true;
         }
         event.preventDefault();
         event.stopImmediatePropagation();
@@ -32,6 +40,7 @@
     pointerover pointerenter pointerdown pointermove pointerup pointercancel pointerout pointerleave pointerrawupdate gotpointercapture lostpointercapture
     auxclick click contextmenu dblclick DOMActivate mousedown mouseenter mouseleave mousemove mouseout mouseover mouseup webkitmouseforcechanged webkitmouseforcedown webkitmouseforcewillbegin webkitmouseforceup
     gesturechange gestureend gesturestart touchcancel touchend touchmove touchstart
+    scroll
     `.split(/\s+/).forEach(eventName => {
         document.addEventListener(eventName, handleEvent, { capture: true });
     });
