08fd4a88a6aa44979b7ce6c9c5768b2ecf2432ed
megahomyak <g.megahomyak@gmail.com>
2024-09-22 13:13:39 +0500

    A new, simpler method of playing sound

--- content.js
+++ content.js
@@ -2,14 +2,7 @@
     if (browser === undefined) {
         var browser = chrome;
     }
-    const AudioContext = window.AudioContext// || window.webkitAudioContext;
-    const audioContext = new AudioContext();
-
-    const popAudioBuffer = await audioContext.decodeAudioData(
-        await (
-            await fetch(browser.runtime.getURL('pop.mp3'))
-        ).arrayBuffer()
-    );
+    const pop = new Audio(browser.runtime.getURL('pop.mp3'));
 
     let extension = {
         _isEnabled: null,
@@ -34,22 +27,19 @@
         }
         event.preventDefault();
         event.stopImmediatePropagation();
-        (async () => {
-            if (event.type === "pointerup") {
-                if (selectedElement !== document.documentElement) {
-                    const audioBufferSource = audioContext.createBufferSource();
-                    audioBufferSource.buffer = popAudioBuffer;
-                    audioBufferSource.connect(audioContext.destination);
-                    await audioContext.resume();
-                    audioBufferSource.start();
-                    selectedElement.remove();
-                    console.log(audioContext, audioBufferSource);
-                }
-            } else if (event.type === "pointerdown") {
-                event.target.style["touch-action"] = "manipulation";
-                selectedElement = event.target;
+        if (event.type === "pointerup") {
+            if (selectedElement !== document.documentElement) {
+                let popClone = pop.cloneNode();
+                popClone.play();
+                popClone.addEventListener("ended", () => {
+                    popClone.remove();
+                });
+                selectedElement.remove();
             }
-        })();
+        } else if (event.type === "pointerdown") {
+            event.target.style["touch-action"] = "manipulation";
+            selectedElement = event.target;
+        }
     }
 
     `
